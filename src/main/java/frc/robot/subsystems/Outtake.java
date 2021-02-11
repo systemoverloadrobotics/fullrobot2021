65ec14cffe044982b6933bbc3e20c5a2ac4192fb
yoshiyahoo <josiahtroup@gmail.com>
2021-02-10 19:27:26 -0800

    Added tv value

--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -14,13 +14,13 @@ public class Outtake extends SubsystemBase{
 
 
     NetworkTable networkTable = NetworkTableInstance.getDefault().getTable("Limelight");
+    NetworkTableEntry tv = networkTable.getEntry("tv"); //Whether the limelight has any valid targets (0 or 1)
     NetworkTableEntry tx = networkTable.getEntry("tx"); //Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
     NetworkTableEntry ty = networkTable.getEntry("ty"); //Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
     NetworkTableEntry ta = networkTable.getEntry("ta"); //Target Area (0% of image to 100% of image)
 
     public void GetLimeLightData(){
         
-
     }
 
 }
