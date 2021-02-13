1745360365af2df39f7528a2c4904c695393e031
yoshiyahoo <josiahtroup@gmail.com>
2021-02-12 19:43:31 -0800

    Converting to angles was unnessecary. It is now removed

--- src/main/java/frc/robot/subsystems/LimelightManager.java
+++ src/main/java/frc/robot/subsystems/LimelightManager.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
 public class LimelightManager implements ILimelightManager{
-    
+
     private double tx;
     private double ty;
     private double ta;
@@ -24,28 +24,4 @@ public class LimelightManager implements ILimelightManager{
     public double getA() {
         return ta;
     }
-
-    @Override
-    public double setX() {
-        return 0;
-    }
-
-    @Override
-    public double setY() {
-        return 0;
-    }
-
-    @Override
-    public double setA() {
-        return 0;
-    }
-
-    @Override
-    public void ConvertPixelsToAngles() {
-        double nx = (1/160) * (getX() - 159.5); 
-        double ny = (1/120) * (119.5 - getY());
-
-        
-    }
-
 }
