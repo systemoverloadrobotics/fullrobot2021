170872f29c9db186ee0ca1a50ebde86e850d40a9
yoshiyahoo <josiahtroup@gmail.com>
2021-02-12 18:55:29 -0800

    Update ILimelightManager.java

--- src/main/java/frc/robot/subsystems/ILimelightManager.java
+++ src/main/java/frc/robot/subsystems/ILimelightManager.java
@@ -3,13 +3,17 @@ package frc.robot.subsystems;
 public interface ILimelightManager {
     
     
-    public double getX;
-    public double getY;
-    public double getA;
+    public double getX(); 
+    public double getY();
+    public double getA();
 
 
-    public double setX;
-    public double setY;
-    public double setA;
+    public double setX();
+    public double setY();
+    public double setA();
+
+
+
+    public void ConvertPixelsToAngles();
 
 }
